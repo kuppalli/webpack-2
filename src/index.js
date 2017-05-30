c31dd35c9713e93fb39c7f22c42c2cba3db94404
Sridhar <sridhar.kuppalli@gmail.com>
2017-05-30 14:11:17 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require("./assets/stylesheets/styles.scss");
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/App.jsx";
+
+const ticker = () => {
+  ReactDOM.render(
+    <App userDisplayName="Jhon" />,
+    document.getElementById("root")
+  );
+};
+
+setInterval(ticker, 1000);
+
+console.log("hello webpack, i am watching for changes now");
+
+
+
+// test if the ES6 is working
+
+// const dress = () => {
+//     document.write('sample message printing here');
+// }
+// dress();
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
+
+let config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.scss', '.css', '.jpeg', '.jpg', '.gif', '.png'],
+        alias: {
+            // Create aliases
+            // when you say import somthing from images/... go and look for src/assets/images folders
+            images: path.resolve(__dirname, 'src/assets/images')  // src/assets/images alias
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                //loader: ['style-loader', 'css-loader', 'sass-loader']
+                use: ['css-hot-loader'].concat(ExtractTextWebpackPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                    fallback: 'style-loader'
+                }))
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: ['file-loader?context=src/assets/images/&name=images/[path][name].[ext]', {
+                    // images loader
+                    loader: 'image-webpack-loader',
+                    query: {
+                        mozjpeg: {
+                            progressive: true,
+                        },
+                        gifsicle: {
+                            interlaced: false,
+                        },
+                        optipng: {
+                            optimizationLevel: 4,
+                        },
+                        pngquant: {
+                            quality: '75-90',
+                            speed: 3,
+                        },
+                    },
+                }],
+                exclude: /node_modules/,
+                include: __dirname,
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin('styles.css')
+    ],
+    //devtool: 'eval-source-map' //TODO: this looks costly when doing the prod build 
+    devServer: {
+        contentBase: path.resolve(__dirname, './public'),
+        historyApiFallback: true,
+        inline: true,
+        open: true
+    }
+
+}
+
+module.exports = config;
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeCSSAssets()
+    );
+}
