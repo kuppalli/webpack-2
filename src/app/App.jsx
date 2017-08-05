4030d1d57d788c71c4a734423b83f199fa45c74d
Sridhar Kuppalli Narayanaswamy <snarayanaswamy@sapient.com>
2017-08-05 19:40:42 -0400

    formatting with prettier

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import sample1 from 'images/sample1.png';
-import sample2 from 'images/sample2.png';
-import sample3 from 'images/sample3.jpg';
+import React, { Component } from "react";
+import sample1 from "images/sample1.png";
+import sample2 from "images/sample2.png";
+import sample3 from "images/sample3.jpg";
 
-const hello = (props) => {
-    return (
-        <div>
-            <h5 className="heading">Hello <em>{props.userDisplayName}</em> welcome to react component !!! </h5>
-            <code>
-                local time : {new Date().toLocaleTimeString()}
-            </code>
-             <br/>
-            <img src={sample3} />
-        </div>
-    );
-}
+const hello = props => {
+  return (
+    <div>
+      <h5 className="heading">
+        Hello <em>{props.userDisplayName}</em> welcome to react component !!!{" "}
+      </h5>
+      <code>
+        local time : {new Date().toLocaleTimeString()}
+      </code>
+      <br />
+      <img src={sample3} />
+    </div>
+  );
+};
 
 export default hello;
