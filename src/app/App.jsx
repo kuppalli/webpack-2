6832865888af2e7e466dc3cd3994b86fe258fa4d
Sridhar Narayanaswamy <snarayanaswamy@sapient.com>
2019-03-25 16:49:07 -0500

    doing some cleanup

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
-import sample1 from "images/sample1.png";
-import sample2 from "images/sample2.png";
-import sample3 from "images/sample3.jpg";
+import React from "react";
+import sample1 from "images/sample3.jpg";
 
 const hello = props => {
   return (
     <div>
-      <h5 className="heading">
+      <h5 className='heading'>
         Hello <em>{props.userDisplayName}</em> welcome to react component !!!{" "}
       </h5>
-      <code>
-        local time : {new Date().toLocaleTimeString()}
-      </code>
+      <code>local time : {new Date().toLocaleTimeString()}</code>
       <br />
-      <img src={sample3} />
+      <img src={sample1} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ require("./assets/stylesheets/styles.scss");
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App.jsx";
+import { cube } from "./app/maths";
 
 const ticker = () => {
   ReactDOM.render(
-    <App userDisplayName="Jhon" />,
+    <App userDisplayName='Jhon' />,
     document.getElementById("root")
   );
 };
@@ -15,9 +16,10 @@ setInterval(ticker, 1000);
 
 console.log("hello webpack, i am watching for changes now");
 
-// test if the ES6 is working
+console.log("cube of 3 is ", cube(3));
 
-// const dress = () => {
-//     document.write('sample message printing here');
-// }
-// dress();
+// spread operator 
+const obj1 = { firstName: 'Foo', age: 22 };
+const obj2 = { lastName: 'Bar', gender: 'M' };
+const newObj = { ...obj1, ...obj2, planet: 'Earth' };
+console.log('new object ', newObj);
